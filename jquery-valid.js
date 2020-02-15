aa9cd3e6e4a082b577112ddf231398c4bd2c06c3
Aftab <aftaba@mindfiresolutions.com>
2020-02-15 16:23:58 +0530

    Added more feature

--- jquery-valid.js
+++ jquery-valid.js
@@ -1,28 +1,99 @@
+var error = false;
 $(document).ready( function(){
 	$('form.data-validation-check').submit( function( e){
-		$('.valid-err').remove();
-		e.preventDefault();
+		$('.validate-error').remove();
 		var formElements = $( '*[data-validation]');
 		$.each( formElements, function( i, formElement) {
 			validationRule = $(this).attr('data-validation');
 			validationRuleArray = validationRule.split('|');
+			console.log( validationRuleArray );
 			for( i=0; i<validationRuleArray.length; i++ ) {
 				if ( validationRuleArray[i] == "required" ) {
-					valid_required( formElement );
+					validate_required( formElement, validationRuleArray[i] );
+				} 
+				if ( validationRuleArray[i].indexOf( "min:") >= 0 ) {
+					validate_min( formElement, validationRuleArray[i] );
+				}
+				if ( validationRuleArray[i].indexOf( "max:") >= 0 ) {
+					validate_max( formElement, validationRuleArray[i] );
+				}
+				if ( validationRuleArray[i] == "special" ) {
+					validate_special( formElement, validationRuleArray[i] );
+				}
+				if ( validationRuleArray[i] == "email" ) {
+					validate_email( formElement, validationRuleArray[i] );
+				}
+				if ( validationRuleArray[i] == "phone" ) {
+					validate_phone( formElement, validationRuleArray[i] );
+				}
+				if ( validationRuleArray[i] == "number" ) {
+					validate_number( formElement, validationRuleArray[i] );
 				}
 			}
 		});
+		if ( error ) {
+			return false;
+		}
+		return true;
 	});
 });
 
 
-function valid_required( formElement ) {
+function validate_required( formElement, rule ) {
 	if ( "" == formElement.value ) {
-		$('<span class="valid-err">This field is required</span>').insertAfter( formElement );
+		error = true;
+		$('<span class="validate-error">This field is required</span>').insertAfter( formElement );
+	} else {
+		error = false;
 	}
 }
-function valid_min( formElement ) {
-	if ( "" == formElement.value ) {
-		$('<span class="valid-err">This field is required</span>').insertAfter( formElement );
+function validate_min( formElement , rule ) {
+	min_rule = rule.split( ':')[1];
+	if ( formElement.value.length < min_rule ) {
+		error = true;
+		$('<span class="validate-error">Must contain minimum '+min_rule+' character</span>').insertAfter( formElement );
+	} else {
+		error = false;
+	}
+}
+function validate_max( formElement , rule ) {
+	max_rule = rule.split( ':')[1];
+	if ( formElement.value.length > max_rule ) {
+		error = true;
+		$('<span class="validate-error">Must contain maximum '+max_rule+' character</span>').insertAfter( formElement );
+	} else {
+		error = false;
+	}
+}
+function validate_special( formElement, rule ) {
+	if( /^[a-zA-Z0-9- ]*$/.test(formElement.value) == true ) {
+		error = true;
+		$('<span class="validate-error">Must contain special character</span>').insertAfter( formElement );
+	} else {
+		error = false;
+	}
+}
+function validate_email( formElement, rule ) {
+	if( /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(formElement.value) == false ) {
+		error = true;
+		$('<span class="validate-error">Must be a valid email address</span>').insertAfter( formElement );
+	} else {
+		error = false;
+	}
+}
+function validate_phone( formElement, rule ) {
+	if( /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/.test(formElement.value) == false ) {
+		error = true;
+		$('<span class="validate-error">Must be a valid phone number</span>').insertAfter( formElement );
+	} else {
+		error = false;
+	}
+}
+function validate_number( formElement, rule ) {
+	if( /^\d+$/.test(formElement.value) == false ) {
+		error = true;
+		$('<span class="validate-error">Must be a number</span>').insertAfter( formElement );
+	} else {
+		error = false;
 	}
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
-jQuery Validator
+<h3>jQuery Validator<h3>
 
-Create a form element with class name as data-validation-check.
-Eg : <form class="data-validation-check" />
+Create a form element with class name as data-validation-check. <br />
+Eg : &lt;form class="data-validation-check" /&gt;
 
-In your form element add a custom data-validation rule.
-Eg: <input type="text" data-validation="required|min:8|max:10" />
+In your form element add a custom data-validation rule. <br />
+Eg: &lt;input type="text" data-validation="required|min:8|max:10|special" /&gt;
+<br />
+Use pipe operator (|) to speate different validation rules.
+
+<strong>Validation Rules </strong><br />
+required  - To validate if field has some value<br />
+min:8 - To validate if field length has minimum 8 character<br />
+max:12 - To validate if field length has maximum 12 character<br />
+phone - To validate if field contains a valid phone number<br />
+email - To validate if field contains a valid email id <br/> 
+special - To validate if field has a special character <br/>
